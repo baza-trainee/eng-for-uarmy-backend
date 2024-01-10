90085af69d6bd2a02586a1000718a2ed81f8208d
Antifishka <101981622+Antifishka@users.noreply.github.com>
2024-01-10 13:09:25 +0200

    add fonts for email

--- src/helpers/markupEmail.js
+++ src/helpers/markupEmail.js
@@ -2,6 +2,27 @@ const markupEmail = (newPassword) =>
     `<html>
         <head>
             <style>
+                @font-face {
+                    font-family: 'Furore';
+                    src: url('https://eng-for-uarmy.onrender.com/fonts/Furore.woff2') format('truetype');
+                    font-weight: 400;
+                    font-style: normal;
+                }
+                @font-face {
+                    font-family: 'Arial';
+                    src: url('https://eng-for-uarmy.onrender.com/fonts/ArialMT.woff2') format('truetype');
+                    font-weight: 400;
+                    font-style: normal;
+                }
+                @font-face {
+                    font-family: 'Arial-Bold';
+                    src: url('https://eng-for-uarmy.onrender.com/fonts/Arial-BoldMT.woff2') format('truetype');
+                    font-weight: 700;
+                    font-style: normal;
+                }
+                body {
+                    font-family: 'Arial', sans-serif;
+                }
                 h2,
                 p {
                     margin: 0;
@@ -20,11 +41,23 @@ const markupEmail = (newPassword) =>
                 width='222px
                 height='92px'
                 style='margin: 0 auto 66px;'/>
+
             <h2 style='margin-bottom: 40px; font-family: Furore; font-size: 30px; color: #231F20;'>
                 Вас вітає Eng for UArmy
             </h2>
-            <p style='margin-bottom: 40px; font-family: Arial; font-size: 18px; color: #231F20;'>Ми отримали Ваш запит на відновлення паролю для входу в адміністративну частину сайту. Для того, щоб увійти в обліковий запис, скористайтеся новим паролем.</p>
-            <div style='font-size: 22px;'>
+
+            <p style='margin-bottom: 40px;
+                font-family: Arial;
+                font-size: 18px;
+                line-height: 1.5;
+                color: #231F20;'>
+                Ми отримали Ваш запит на відновлення паролю для входу в адміністративну частину сайту. Для того, щоб увійти в обліковий запис, скористайтеся новим паролем.
+            </p>
+
+            <div style='font-size: 22px;
+                font-weight: 700;
+                line-height: 1.5;
+                letter-spacing: -0.408px;'>
                 <span style='margin-right: 8px; color: #231F20;'> Ваш новий пароль: </span>
                 <span style='color: #798738;'>${newPassword}</span>
             </div>
